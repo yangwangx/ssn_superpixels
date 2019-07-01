83ca72049a8439d26b6296cabe1c6ea3bcf6d382
Yang Wang <yangwangx@gmail.com>
2019-07-01 10:57:44 -0400

    add a script saving caffe model as numpy array

---
+++ save_caffe_model_as_nparray.py
@@ -0,0 +1,32 @@
+from init_caffe import *
+from create_net import load_ssn_net
+
+models = [
+    './models/ssn_bsds_model.caffemodel',
+    # './models/ssn_cityscapes_model.caffemodel',
+    # './models/ssn_sintel_model.caffemodel',
+    # './models/ssn_voc_model.caffemodel',
+]
+
+for caffe_model in models:
+    net = load_ssn_net(201, 201, 100, 1.0, 1.0, 10, 10, 5)
+    net.copy_from(caffe_model)
+
+    layers = sorted(net.params.keys())
+
+    # conv layers
+    conv_params = []
+    for layer in layers:
+        if 'Convolution' in layer:
+            conv_params.append(net.params[layer][0].data)
+            conv_params.append(net.params[layer][1].data)
+
+    # bn layers
+    bn_params = []
+    for layer in layers:
+        if 'BatchNorm' in layer:
+            bn_params.append(net.params[layer][0].data)
+            bn_params.append(net.params[layer][1].data)
+    
+    import numpy as np
+    np.savez(caffe_model+'.npz', conv_params, bn_params)
